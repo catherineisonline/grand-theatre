f70ec2bbbd3f3a9fe2d4cd4127b256bb87e34edf
Ekaterine (Catherine) Mitagvaria <63928803+catherineisonline@users.noreply.github.com>
2022-06-09 11:19:42 +0400

    first commit

---
+++ javascript.js
@@ -0,0 +1,14 @@
+           
+              // ****** HAMBURGER MENU ******
+
+  const hamburger = document.getElementById('hamburger');
+  const navUl = document.getElementById('nav-ul');
+
+  hamburger.addEventListener('click', () => {
+  	navUl.classList.toggle('show'); 
+  });
+
+
+
+
+
