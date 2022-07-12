c6a51bf793173f33e846245db9c2373a5f6188f0
Ekaterine (Catherine) Mitagvaria <63928803+catherineisonline@users.noreply.github.com>
2022-07-12 15:39:21 +0400

    updated header

--- javascript.js
+++ javascript.js
@@ -1,14 +1,8 @@
-           
-              // ****** HAMBURGER MENU ******
+// ****** HAMBURGER MENU ******
 
-  const hamburger = document.getElementById('hamburger');
-  const navUl = document.getElementById('nav-ul');
+const hamburger = document.getElementById("hamburger");
+const navUl = document.getElementById("nav-ul");
 
-  hamburger.addEventListener('click', () => {
-  	navUl.classList.toggle('show'); 
-  });
-
-
-
-
-
+hamburger.addEventListener("click", () => {
+  navUl.classList.toggle("show");
+});
